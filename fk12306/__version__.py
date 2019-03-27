5b385691f643a503d9f859acb89d5d8bffea95ad
HJK <HJKdev@gmail.com>
2019-03-27 15:55:15 +0800

    v0.1.1

--- fk12306/__version__.py
+++ fk12306/__version__.py
@@ -9,7 +9,7 @@
 __title__ = "fk12306"
 __description__ = "12306查票助手，一键查询沿途所有站点，先上车后补票，暂不支持抢票"
 __url__ = "https://github.com/0xHJK/fk12306"
-__version__ = "0.1.0"
+__version__ = "0.1.1"
 __author__ = "HJK"
 __author_email__ = "HJKdev@gmail.com"
 __license__ = "MIT License"
