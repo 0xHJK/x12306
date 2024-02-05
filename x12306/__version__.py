5ae4a588422fad599385a751e15aaf8c44fe4209
0xHJK <HJKdev@gmail.com>
2024-02-05 20:46:14 +0800

    update version

--- x12306/__version__.py
+++ x12306/__version__.py
@@ -7,10 +7,10 @@
 """
 
 __title__ = "x12306"
-__description__ = "12306查票助手，一键查询沿途所有站点，先上车后补票，暂不支持抢票"
+__description__ = "12306查票助手，一键查询沿途所有站点，在买不到票的时候，先上车后补票，暂不支持抢票"
 __url__ = "https://github.com/0xHJK/x12306"
-__version__ = "0.1.1"
+__version__ = "0.2.0"
 __author__ = "HJK"
 __author_email__ = "HJKdev@gmail.com"
 __license__ = "MIT License"
-__copyright__ = "Copyright 2019 HJK"
+__copyright__ = "Copyright 2024 HJK"
